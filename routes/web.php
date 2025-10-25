d7f1567bb76893922680d39949e906a88eef54ed
uitgeteld <66201378+uitgeteld@users.noreply.github.com>
2025-10-25 22:37:47 +0000

    Updated all the routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\UserController;
+
+use App\Http\Middleware\IsAdmin;
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
+
+Route::resource('users', UserController::class)->middleware(IsAdmin::class);
