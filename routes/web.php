86819f876f84dcb19243e74728e6c5c4860ea77d
uitgeteld <swendewit07@gmail.com>
2026-02-19 19:26:50 +0100

    Added tree page

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,16 @@ use App\Http\Middleware\IsAdmin;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+Route::get('/tree', function () {
+    return view('tree');
+})->name('tree');
 Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::view('/dashboard', 'dashboard.index')->name('dashboard');
     Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
     Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
-    
-    // Profile routes
+
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::get('/profile/two-factor', [ProfileController::class, 'showTwoFactor'])->name('profile.two-factor');
 });
