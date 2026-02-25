3bec756b358660d83841fafc29be83da188edaab
uitgeteld <swendewit07@gmail.com>
2026-02-25 16:26:52 +0100

    Added a back button on login page

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,5 +1,11 @@
 <x-layout>
 
+    <a href="javascript:history.back()" class="fixed top-3 left-3 p-3 text-gray-600 hover:text-gray-900 transition z-50">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M19 12H5M12 19l-7-7 7-7"/>
+        </svg>
+    </a>
+
     <div class="min-h-screen flex items-center justify-center px-4">
         <div class="w-full max-w-md">
             <div class="mb-8">
